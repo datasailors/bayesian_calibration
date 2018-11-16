5ffcc5e598a8cc4764dcca90829d1c29898bf1e5
fabiononato <26128549+fabiononato@users.noreply.github.com>
2018-11-15 23:29:17 -0800

    Update startup.sh - solve crashing on Ubuntu.

--- Container-Root/startup.sh
+++ Container-Root/startup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # Container startup script
-/bin/bash -c "jupyter notebook --allow-root --ip='*' --no-browser --NotebookApp.token=''"
+/bin/bash -c "jupyter notebook --allow-root --ip='0.0.0.0' --no-browser --NotebookApp.token=''"
 #./run_jupyter.sh --allow-root
